b6fb37d45e20fd13e08f940ab72fb67549c39d9d
Ann Ndiga <annndiga@gmail.com>
2023-07-02 19:05:08 +0300

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,66 @@
 /* Your Code Here */
-
+// Function to create an employee record
+function createEmployeeRecord(arr) {
+    return {
+      firstName: arr[0],
+      familyName: arr[1],
+      title: arr[2],
+      payPerHour: arr[3],
+      timeInEvents: [],
+      timeOutEvents: []
+    };
+  }
+  
+  // Function to create employee records from an array of arrays
+  function createEmployeeRecords(arrays) {
+    return arrays.map(createEmployeeRecord);
+  }
+  
+  // Function to create a time-in event for an employee
+  function createTimeInEvent(dateStamp) {
+    const [date, hour] = dateStamp.split(" ");
+    this.timeInEvents.push({
+      type: "TimeIn",
+      date,
+      hour: parseInt(hour, 10)
+    });
+    return this;
+  }
+  
+  // Function to create a time-out event for an employee
+  function createTimeOutEvent(dateStamp) {
+    const [date, hour] = dateStamp.split(" ");
+    this.timeOutEvents.push({
+      type: "TimeOut",
+      date,
+      hour: parseInt(hour, 10)
+    });
+    return this;
+  }
+  
+  // Function to calculate the hours worked on a specific date
+  function hoursWorkedOnDate(date) {
+    const timeIn = this.timeInEvents.find(event => event.date === date);
+    const timeOut = this.timeOutEvents.find(event => event.date === date);
+    return (timeOut.hour - timeIn.hour) / 100;
+  }
+  
+  // Function to calculate the wages earned on a specific date
+  function wagesEarnedOnDate(date) {
+    const hoursWorked = hoursWorkedOnDate.call(this, date);
+    return hoursWorked * this.payPerHour;
+  }
+  
+  // Function to find an employee by first name in a collection of employee records
+  function findEmployeeByFirstName(collection, firstName) {
+    return collection.find(employee => employee.firstName === firstName);
+  }
+  
+  // Function to calculate the total payroll for a collection of employee records
+  function calculatePayroll(records) {
+    return records.reduce((totalPayroll, record) => totalPayroll + allWagesFor.call(record), 0);
+  }
+  
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
